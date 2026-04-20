802b3735023403b5213dffa60173680eacb0ae50
Lesnier Gonzalez Lopez <lesniergonzalez@gmail.com>
2026-04-19 22:24:54 -0500

    Politica de Privacidad y Redes sociales

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -6,70 +6,64 @@
         <h2>Portafolio</h2>
         <p>Nuestro Portafolio</p>
       </div>
-      <!-- 
-      <div class="row" data-aos="fade-up" data-aos-delay="100">
-        <div class="col-lg-12 d-flex justify-content-center">
-          <ul id="portfolio-flters">
-            <li data-filter="*" class="filter-active">All</li>
-            <li data-filter=".filter-app">Web</li>
-            <li data-filter=".filter-card">App</li>
-            <li data-filter=".filter-web">VR/Game</li>
-          </ul>
-        </div>
-      </div> -->
 
-      <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+      <div
+        ref="portfolioContainer"
+        class="row portfolio-container"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="Proyecto 1" />
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="Proyecto 2" />
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="Proyecto 3" />
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-card">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="Proyecto 4" />
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="Proyecto 5" />
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="Proyecto 6" />
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-card">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="Proyecto 7" />
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-card">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="Proyecto 8" />
           </div>
         </div>
 
         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
           <div class="portfolio-wrap shadow">
-            <img src="/assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="" />
+            <img src="/assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="Proyecto 9" />
           </div>
         </div>
       </div>
@@ -79,4 +73,30 @@
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+import Isotope from 'isotope-layout'
+import imagesLoaded from 'imagesloaded'
+
+const portfolioContainer = ref(null)
+let iso = null
+
+onMounted(() => {
+  if (!portfolioContainer.value) return
+
+  // Usar imagesLoaded garantiza que Isotope calcule alturas reales
+  // y no cero (que era el bug en producción al primer load)
+  imagesLoaded(portfolioContainer.value, () => {
+    iso = new Isotope(portfolioContainer.value, {
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows',
+    })
+  })
+})
+
+onUnmounted(() => {
+  if (iso) {
+    iso.destroy()
+    iso = null
+  }
+})
 </script>
